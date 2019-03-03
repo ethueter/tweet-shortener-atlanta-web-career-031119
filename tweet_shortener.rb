c05276f4f88a8ceb953401f5661b317ba9573b5d
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-03 19:46:36 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,3 +1,11 @@
 def dictionary
   dictionary= {"hello" => "hi", "to, two, too" => '2', "for, four" => '4','be' => 'b', 'you'=>'u', "at" => "@", "and" => "&"}
 end
+
+def word_substituter(tweet)
+  dictionary
+  list = dictionary.keys
+  new_array = tweet.split
+  new_array.each do |x|
+    
+end 
