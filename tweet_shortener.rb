fa97bf5e9cbc4d323640a7390222a295013b6267
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-03 22:02:17 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -23,7 +23,8 @@ def bulk_tweet_shortener(array_of_tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  if tweet.length < 130
+  test = tweet.to_s 
+  if test.length < 130
     puts tweet
   else
     puts tweet[0..137] + "..."
