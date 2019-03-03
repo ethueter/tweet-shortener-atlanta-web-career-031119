973dcc7c4c59b81d24801800e889a29e409c9aa8
Tyler Hueter <tylerhueter08@gmail.com>
2019-03-03 21:10:15 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -5,18 +5,7 @@ def dictionary
 end
 
 def word_substituter(tweet)
-  dictionary
-  list = dictionary.keys
-  list_2 = list.join(", ")
-  list_3 = list_2.split(", ")
-  new_array = tweet.split
-  new_array.collect! do |x|
-    if (list_3.include?(x))
-      dictionary[x]
-    else
-      x 
-       binding.pry 
-    end 
-  end 
-  new_array.join(" ")  
+  tweet.split.collect |word|
+  if dictionary.key.include?(word)
+    
 end 
